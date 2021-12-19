4836ca6785acf505baeea92d5919bbba677b156b
Виктор Редькин <Moridin830@gmail.com>
2021-12-19 19:54:49 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    class Task1
+    {
+        static string PathToCatatlog = "D:/Learning/SkillFactory/Module 8/Work with files";
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void EraseFiles()
+        {
+            if (Directory.Exists(PathToCatatlog)){
+
+            }
+        }
+    }
+}
